b86f6dbe1ab4744b4cdeaab30f402414a2677eb4
Karamjit Judge <karamjit1.judge@gmail.com>
2023-04-10 01:24:12 +0000

    Update 3 files

--- src/Food.js
+++ src/Food.js
@@ -1,3 +1,33 @@
+import { Button } from "bootstrap"
+
 export default function Food(){
-    return<div>Food page</div>
+    return<div>
+        <div>Food page
+        </div>
+    <div>
+    <button>Fried Chicken</button>
+   </div>
+   <div>
+    <button>Salad</button>
+    </div>
+    <div>
+    <button>Pizza</button>
+    </div>
+    <div>
+    <button href = "App.css">French Fries</button>
+    </div>
+        
+    </div>
+    
+
+}
+
+function listFood(){
+    let foods = [
+        {"Name": "3 Piece Fried Chicken", 
+        "Price": 5.99,
+        "Spice Level": 0,
+        "Description": "Fried Chicken with chickens from the Miyagi prefecture"
+        },
+        ]
 }
